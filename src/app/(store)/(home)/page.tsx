2b4b01975a02cee515c7e50c2e214b3c43da1279
Andre luiz <adventureandre@hotmail.com>
2024-07-15 20:26:21 -0300

    configuration the Projeto Base

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,18 +1,3 @@
-import { api } from '@/data/api'
-
-export default async function Home() {
-  const response = await api('/user', {
-    // cache:'no-store'
-    next: {
-      revalidate: 60 * 60, // 1hour
-    },
-  })
-  const data = await response.json()
-
-  return (
-    <>
-      <div>{data[0].name}</div>
-      <div>{data[0].email}</div>
-    </>
-  )
+export default function Home() {
+  return <div>aki</div>
 }
--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,10 +1,13 @@
-import Header from '@/components/Header'
+import { Metadata } from 'next'
 import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Home',
+}
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto min-h-screen w-full max-w-[1600px] ">
-      <Header />
       {children}
     </div>
   )
