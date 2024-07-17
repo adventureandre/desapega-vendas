e7d35eb53443e08fdeeac34d8716919ce889f2d1
Andre luiz <adventureandre@hotmail.com>
2024-07-16 21:28:25 -0300

    creating carousel

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -90,8 +90,8 @@ export default function Home() {
           </ul>
         </nav>
       </header>
-      <main className="space-y-6">
-        <section className="mx-[250px] space-y-6">
+      <main className="space-y-6 flex justify-center">
+        <section className="mx-[250px] space-y-6 min-w-[400px]">
           <div className="py-4 border-b border-gray-400">
             <div className="flex justify-between items-center ">
               <h2 className="text-3xl text-zinc-400">Novos</h2>
