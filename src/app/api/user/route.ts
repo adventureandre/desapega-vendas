8715c01b137565eb73aa851b4147215e8c77092e
Andre luiz <adventureandre@hotmail.com>
2024-09-02 19:39:51 -0300

    test: cors

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,27 @@
 import { prisma } from '@/lib/prisma'
+import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'http://desapegavendas.com.br',
+  ]
+
+  const origin = headers().get('origin') || ''
+
+  console.log(origin)
+
+  if (!allowedOrigins.includes(origin)) {
+    return NextResponse.json(
+      { error: 'CORS: Origin not allowed' },
+      {
+        status: 403, // Forbidden
+      },
+    )
+  }
+
   try {
     const allUsers = await prisma.user.findMany()
     return NextResponse.json(allUsers, {
