161c2b74374ff124c8212526c08694082d212ebf
Andre luiz <adventureandre@hotmail.com>
2024-09-08 20:56:43 -0300

    teste:cors

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -16,7 +16,7 @@ export async function GET() {
 
   if (!allowedOrigins.includes(origin)) {
     return NextResponse.json(
-      { error: origin },
+      { error: 'Erro cors seu ip e: ' + origin },
       {
         status: 403, // Forbidden
       },
