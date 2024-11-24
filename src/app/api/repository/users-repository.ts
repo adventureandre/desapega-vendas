8f99cc0458759070ec07b7b0b0149f967937b269
André Luiz <adventureandre@hotmail.com>
2024-11-24 15:55:11 -0300

    feat: creating first repository

---
+++ src/app/api/repository/users-repository.ts
@@ -0,0 +1,6 @@
+import { Prisma, User } from '@prisma/client'
+
+export interface UsersRepository {
+  create(data: Prisma.UserCreateInput): Promise<User>
+  findById(id: string): Promise<User | null>
+}
